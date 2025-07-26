666a655fe32927d2d9f520924ad36e6d5a5c6764
git <client@funxdata.com>
2025-07-27 00:01:26 +0800

    watch file upload process

--- src/public.js
+++ src/public.js
@@ -5,5 +5,5 @@ const dir_files = await readDirFiles('./public')
 for (let index = 0; index < dir_files.length; index++) {
     const element = dir_files[index];
     let res = await UpfileToUpYun("uifonts",element)
-    // console.log(res)
+    console.log(res)
 }
