1f99bdcce9cd3617d64c38b57cc5b11b8fedcb5c
git <client@funxdata.com>
2025-07-27 00:07:45 +0800

    watch up public dir

--- src/public.js
+++ src/public.js
@@ -1,7 +1,8 @@
 import { UpfileToUpYun,readDirFiles } from "./upfiles.js";
 
 // 示例用法
-const dir_files = await readDirFiles('./public')
+const dir_files = await readDirFiles('./public/')
+console.log(dir_files)
 for (let index = 0; index < dir_files.length; index++) {
     const element = dir_files[index];
     let res = await UpfileToUpYun("uifonts",element)
